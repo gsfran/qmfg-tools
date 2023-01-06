b90bee45ea808895efe59827104da4d75ad9c820
gfrancis-dev <garrett.francis@quidel.com>
2023-01-06 06:34:38 -0800

    more web dev stuff

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import jyserver.Flask as jsf
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@jsf.use(app)
+class App:
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        self.js.document.getElementById('count').innerHTML = self.count
+
+@app.route('/')
 def index():
-    return render_template('test.html')
+    return App.render(render_template('index.html'))
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
