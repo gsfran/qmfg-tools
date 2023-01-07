3cf750878011ba854fbbb5b50698e742256803b2
gfrancis-dev <garrett.francis@quidel.com>
2023-01-06 22:37:50 -0800

    building out webapp scheduler

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import jyserver.Flask as jsf
 from flask import Flask, render_template
 
@@ -17,8 +18,9 @@ class App:
 def index():
     return App.render(render_template('index.html'))
 
+
 if __name__ == '__main__':
 
-    app.run(host='10.25.20.153', port=5000, debug=True)
-
-    # app.run(host='127.0.0.1', port=5000, debug=True)
+    # app.run(host='10.25.20.153', port=5000, debug=True)
+   
+    app.run(host='127.0.0.1', port=8080, debug=True)
