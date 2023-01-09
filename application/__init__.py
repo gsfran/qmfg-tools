c719a42348fc882c6ab9f2b2d3c1cb59b567740a
gfrancis-dev <garrett.francis@quidel.com>
2023-01-09 15:27:16 -0800

    .

--- application/__init__.py
+++ application/__init__.py
@@ -2,14 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 # implement a hash key pls
 app.config['SECRET_KEY'] = 'andaksfhgfsdghmsDFGDFGMSDGFHmnsgfnmafdgmsdfbs'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testDB.db'
 
 db = SQLAlchemy(app)
 
-with app.app_context():
+@app.before_first_request
+def create_tables():
     db.create_all()
+
+# with app.app_context():
+#     db.create_all()
 
 from application import routes
 # from application.machines import iTrak
