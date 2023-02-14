b59ddb5d61f1a44bd8cefc6ef0bcc69d1bd7b1f0
gfrancis-dev <garrett.francis@quidel.com>
2023-02-14 10:58:56 -0800

    implemented .env file

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv('./.env')
 
 
 class Config(object):
     basedir = os.path.abspath(os.path.dirname(__file__))
-    
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'shh_its_a_secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:///{os.path.join(basedir)}/app.sqlite'
+
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'shh_its_a_secret')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL', 'sqlite:///{os.path.join(basedir)}/app.db')
