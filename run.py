9bf5cb4e46ccde96fcfb8734d193dfbccec54eb8
gfrancis-dev <garrett.francis@quidel.com>
2023-02-14 15:18:44 -0800

    .

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from application import app
 
 
 if __name__ == '__main__':
-    if os.environ['DEBUG'] is True:
+    if os.environ.get('DEBUG') == 'True':
         app.run(host='127.0.0.1', port=5000, debug=True)
     else:
         app.run(host='10.15.5.95', port=5000, debug=False)
