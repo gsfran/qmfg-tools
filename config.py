1c7a945cc9e69dfcb427ea1316c9f4a03c1c076a
gfrancis-dev <garrett.francis@quidel.com>
2023-02-14 11:45:13 -0800

    .

--- config.py
+++ config.py
@@ -7,6 +7,5 @@ load_dotenv('./.env')
 class Config(object):
     basedir = os.path.abspath(os.path.dirname(__file__))
 
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'shh_its_a_secret')
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL', './.test/app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
