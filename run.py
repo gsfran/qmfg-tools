2c29fbdb5ea3b775f417fd4273460a4ebcaa1c58
gfrancis-dev <garrett.francis@quidel.com>
2023-02-14 11:15:54 -0800

    debug fix

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from application import app
 
 
 if __name__ == '__main__':
-    # app.run(host='10.25.20.153', port=5000, debug=True)
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    if os.environ['DEBUG'] is True:
+        app.run(host='127.0.0.1', port=5000, debug=True)
+    else:
+        app.run(host='10.15.5.95', port=5000, debug=True)
