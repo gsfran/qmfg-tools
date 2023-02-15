ea0dea9f0b34208fc02fc72414fb42272224bbf9
gfrancis-dev <garrett.francis@quidel.com>
2023-02-14 18:20:33 -0800

    synced requirements.txt

--- application/vision/_file_handling.py
+++ application/vision/_file_handling.py
@@ -16,19 +16,19 @@ class ProcessData:
 
     @staticmethod
     def load(
-            target_file: str, _data_headers: list
-        ) -> pd.DataFrame:
+        target_file: str, _data_headers: list
+    ) -> pd.DataFrame:
         """
         Returns a Dataframe containing cleaned process data for the given file.
         """
         _t = dt.now()
         print(f'{_t}: Loading data files...\t\t\t', end='')
-        
+
         _path = f'{ProcessData.ROOT_PATH}/{target_file}'
         _raw_data = ProcessData._load_raw_data(
             _path, _data_headers
-            )
-        
+        )
+
         print(f'Done in {dt.now() - _t}')
         return ProcessData._clean_data(_raw_data)
 
@@ -48,11 +48,11 @@ class ProcessData:
         data_ = data_.set_index('datetime')
         data_ = data_.drop(['cognex_timestamp'], axis=1)
 
-        # converts the datetime column to timestamp values, 
+        # converts the datetime column to timestamp values,
         # needed to calculate cycle times
         data_['timestamp'] = (
             data_.index.values.astype(np.int64) / 10 ** 9
-            )
+        )
 
         # adds cycle time and frequency data to the DataFrame
         data_['cycle_time'] = data_['timestamp'].diff()
@@ -82,27 +82,27 @@ class OnlineUtilizationLog:
     ROOT_PATH = '//kansas.us/qfs/Engineering/Shared/Online Utilization Logs'
 
     def load(
-        machine_name:str, month:dt.month, sheets:list = None
-        ) -> pd.DataFrame:
+        machine_name: str, month: dt.month, sheets: list = None
+    ) -> pd.DataFrame:
         """
         Returns data from the machine's Online Utilization Log
         """
 
         first_of_this_month = dt(
             dt.today().year, dt.today().month, 1, 0, 0, 0
-            )
+        )
         file_name = f'{machine_name} Online Utilization.xlsx'
 
         if (month >= first_of_this_month):
             # use /Current Month/ for current month
             folder_path = (
                 f'{OnlineUtilizationLog.ROOT_PATH}/Current Month'
-                )
+            )
         else:
             # use /History/ for any prior months
             folder_path = (
                 f'{OnlineUtilizationLog.ROOT_PATH}/History/{month:%Y/%Y%m}'
-                )
+            )
 
         file_path = f'{folder_path}/{file_name}'
 
@@ -110,15 +110,13 @@ class OnlineUtilizationLog:
             # reads the excel file into DataFrame
             filterwarnings(
                 'ignore', category=UserWarning, module='openpyxl'
-                )
+            )
             try:
                 utilization_log_data = pd.read_excel(
                     excel_reader, sheet_name=sheets, header=None,
                     na_filter=False
-                    )
+                )
             except Exception:
                 raise Exception(f'{file_path} could not be loaded.')
 
         return utilization_log_data
-
-
